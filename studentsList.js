d783aceeb9745d17e51718e695ff3927f0a65d49
Rabin Julien <julienr114@gmail.com>
2017-03-03 11:39:42 +0100

    comment code for some explanetion

--- studentsList.js
+++ studentsList.js
@@ -1,12 +1,30 @@
+// IIFE (Immediately-Invoked Function Expression)
 (function () {
     'use strict'
+
+    // Create HttpRequest
     let getStudent = new XMLHttpRequest();
     getStudent.open("GET", 'students.json', true);
+
+    // When Request state change
     getStudent.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var myArr = JSON.parse(getStudent.responseText);
+
+      // 0:UNSENT 1:OPENED 2:HEADERS_RECEIVED 3:LOADING 4:DONE
+      // this correspond to getStudent
+      if (this.readyState == 4) {
+        // HTTP Status code 200:OK
+        if (this.status == 200) {
+
+            // this.responseText: response content
+            var myArr = JSON.parse(this.responseText); // JSON.parse()
             console.log(myArr);
+
+        } else {
+          alert("Erreur pendant le chargement de la page");
         }
+      }
     };
+
+    // Send request
     getStudent.send();
 }) ();
