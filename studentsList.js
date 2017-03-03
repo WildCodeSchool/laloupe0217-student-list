5171132b684a1023db84e7ff211cd2cd728f5473
Rabin Julien <julienr114@gmail.com>
2017-03-03 11:07:53 +0100

    import & parse students.json in studentlist.js

--- studentsList.js
+++ studentsList.js
@@ -1,1 +1,12 @@
-// Write yout Javascript code in this files
+(function () {
+    'use strict'
+    let getStudent = new XMLHttpRequest();
+    getStudent.open("GET", 'students.json', true);
+    getStudent.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myArr = JSON.parse(getStudent.responseText);
+            console.log(myArr);
+        }
+    };
+    getStudent.send();
+}) ();
