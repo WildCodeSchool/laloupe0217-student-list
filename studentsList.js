4e4b52bce9b3721f79dbb575ec2e3a2767ddd919
Michel Domingos <m.domingos76@gmail.com>
2017-03-06 11:49:14 +0100

    import students.json in studentsList

--- studentsList.js
+++ studentsList.js
@@ -1,1 +1,15 @@
 // Write yout Javascript code in this files
+var request = new XMLHttpRequest();
+
+request.open('GET', 'students.json', true);
+request.onreadystatechange = function() {
+    if (request.readyState == 4) {
+        if (request.status == 200){
+            var students = JSON.parse(request.responseText);
+        console.log(students);
+    } else {
+        dump("Erreur pendant le chargement de la page.\n");
+    }
+  }
+};
+request.send();
