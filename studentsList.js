48087d409b4bf091b17b6a2eb6421fd78e3057c0
Michel Domingos <m.domingos76@gmail.com>
2017-03-06 12:48:38 +0100

    new list elements

--- studentsList.js
+++ studentsList.js
@@ -1,15 +1,32 @@
 // Write yout Javascript code in this files
 var request = new XMLHttpRequest();
+var table = document.getElementById('students');
+
 
 request.open('GET', 'students.json', true);
+
 request.onreadystatechange = function() {
     if (request.readyState == 4) {
-        if (request.status == 200){
+        if (request.status == 200) {
             var students = JSON.parse(request.responseText);
-        console.log(students);
-    } else {
-        dump("Erreur pendant le chargement de la page.\n");
+            console.log(students);
+
+            students.forEach(function(student) {
+                let tr = document.createElement('tr');
+                tr.innerHTML = `<td>${student.firstname}</td>
+                                <td>${student.lastname}</td>
+                                <td>${student.city}</td>
+                                <td>${student['postal-code']}</td>`;
+
+
+                table.appendChild(tr);
+                console.log(student.firstname);
+            });
+
+
+        } else {
+            dump("Erreur pendant le chargement de la page.\n");
+        }
     }
-  }
 };
 request.send();
